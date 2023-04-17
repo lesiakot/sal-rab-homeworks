ee2c6644422a4b80b53921070773daef222aba34
Morogay <lesia.kot@rambler.ru>
2023-04-17 21:16:35 +0300

    Task_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,10 +1,10 @@
 // Исправьте функцию sendRequest
 // Аргументы функции:
-// - имя клиента
-// - телефон клиента
-// - объект с адресом доставки: {street, house, entrance, floor, flat}
-// - список товаров в заказе
-// - стоимость заказа с учетом скидок и доставки
+// - имя клиента name
+// - телефон клиента phone
+// - объект с адресом доставки: {street, house, entrance, floor, flat} address
+// - список товаров в заказе goods
+// - стоимость заказа с учетом скидок и доставки sum
 // Как результат функции требуется вернуть JSON,
 // cформированный в соответствии с правилами:
 // Объект data содержит все данные
@@ -34,20 +34,24 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {
+        client: [name, phone],
+        order: [address, sum],
+        goods: []
+    };
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i ++) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + " , " + address.entrance + " подъезд," + address.floor + " этаж, " + "кв " + address.flat;
+    data.order.sum = sum;
+    
+    data.client = name + " " + phone;
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data:data});
 
     return jsonData;
 }
